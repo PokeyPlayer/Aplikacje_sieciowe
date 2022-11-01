748bde2ca23426b3f6845f6e2c877953487008ad
PokeyPlayer <43058540+PokeyPlayer@users.noreply.github.com>
2022-11-01 17:34:23 +0100

    Add files via upload

---
+++ php_05_obiektowosc/templates_c/f80c158cb7e2358fa570b6f5db8430c9d383ed7f_0.file.CalcView.html.php
@@ -0,0 +1,135 @@
+<?php
+/* Smarty version 3.1.30, created on 2022-11-01 17:17:06
+  from "C:\xampp\htdocs\php_05_obiektowosc\app\CalcView.html" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_63614682afa790_80083173',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'f80c158cb7e2358fa570b6f5db8430c9d383ed7f' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\php_05_obiektowosc\\app\\CalcView.html',
+      1 => 1667319196,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_63614682afa790_80083173 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_loadInheritance();
+$_smarty_tpl->inheritance->init($_smarty_tpl, true);
+?>
+
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_106963190163614682aeb190_86736960', 'footer');
+?>
+
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_84005343863614682afa2e2_97738826', 'content');
+$_smarty_tpl->inheritance->endChild();
+$_smarty_tpl->_subTemplateRender(($_smarty_tpl->tpl_vars['conf']->value->root_path).("/templates/main.html"), $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 2, true);
+}
+/* {block 'footer'} */
+class Block_106963190163614682aeb190_86736960 extends Smarty_Internal_Block
+{
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+Kalkulator kredytowy wersja 2022<?php
+}
+}
+/* {/block 'footer'} */
+/* {block 'content'} */
+class Block_84005343863614682afa2e2_97738826 extends Smarty_Internal_Block
+{
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+
+<h2 class="content-head is-center">Kalkulator kredytowy</h2>
+
+<div class="pure-g">
+<div class="l-box-lrg pure-u-1 pure-u-med-2-5">
+	<form class="pure-form pure-form-stacked" action="<?php echo $_smarty_tpl->tpl_vars['conf']->value->app_url;?>
+/app/calc.php" method="post">
+		<fieldset>
+
+			<label for="id_kwota">Kwota kredytu: </label>
+			<input id="id_kwota" type="text" placeholder="Kwota kredytu" name="kwota" value="<?php echo $_smarty_tpl->tpl_vars['form']->value->kwota;?>
+">
+					
+			<label for="id_lata">Ile lat: </label>
+			<input id="id_lata" type="text" placeholder="Okres spłaty kredytu" name="lata" value="<?php echo $_smarty_tpl->tpl_vars['form']->value->lata;?>
+">
+
+			<label for="id_oprocentowanie">Oprocentowanie kredytu %: </label>
+			<input id="id_oprocentowanie" type="text" placeholder="Oprocentowanie kredytu" name="oprocentowanie" value="<?php echo $_smarty_tpl->tpl_vars['form']->value->oprocentowanie;?>
+">
+
+			<button type="submit" class="pure-button">Oblicz</button>
+		</fieldset>
+	</form>
+</div>
+
+<div class="l-box-lrg pure-u-1 pure-u-med-3-5">
+
+<?php if ($_smarty_tpl->tpl_vars['messages']->value->isError()) {?>
+	<h4>Wystąpiły błędy: </h4>
+	<ol class="err">
+	<?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['messages']->value->getErrors(), 'err');
+if ($_from !== null) {
+foreach ($_from as $_smarty_tpl->tpl_vars['err']->value) {
+?>
+	<li><?php echo $_smarty_tpl->tpl_vars['err']->value;?>
+</li>
+	<?php
+}
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl);
+?>
+
+	</ol>
+<?php }?>
+
+<?php if ($_smarty_tpl->tpl_vars['messages']->value->isInfo()) {?>
+	<h4>Informacje: </h4>
+	<ol class="inf">
+	<?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['messages']->value->getInfos(), 'inf');
+if ($_from !== null) {
+foreach ($_from as $_smarty_tpl->tpl_vars['inf']->value) {
+?>
+	<li><?php echo $_smarty_tpl->tpl_vars['inf']->value;?>
+</li>
+	<?php
+}
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl);
+?>
+
+	</ol>
+<?php }?>
+
+<?php if (isset($_smarty_tpl->tpl_vars['wynik']->value->wynik)) {?>
+	<h4>Miesięczna rata wynosi: </h4>
+	<p class="res">
+	<?php echo $_smarty_tpl->tpl_vars['wynik']->value->wynik;?>
+
+	</p>
+<?php }?>
+
+</div>
+</div>
+
+<?php
+}
+}
+/* {/block 'content'} */
+}
